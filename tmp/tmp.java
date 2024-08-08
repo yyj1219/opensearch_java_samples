d040759197409492ee89ee71678ee2cd0cdd2846
yyj1219 <55434678+yyj1219@users.noreply.github.com>
2024-08-08 17:15:17 +0900

    Update tmp.java

--- tmp/tmp.java
+++ tmp/tmp.java
@@ -1,95 +1,139 @@
 #!/bin/bash
 
 ###################################################
-# MairaDB 11.4.2 installation on Rocky Linux 8
+# Install OpenSearch 2.13.0 using .tar.gz on Rocky Linux 8
 ###################################################
 
-# Extract the VERSION_ID from the release files
+# Extract the VERSION_ID from the OS release files
 version_id=$(grep '^VERSION_ID=' /etc/*-release | cut -d '=' -f 2 | tr -d '"')
 
-# Check if VERSION_ID starts with 8.
+# Verify that the VERSION_ID starts with 8
 if [[ $version_id != 8.* ]]; then
   echo "VERSION_ID does not start with 8. Exiting."
   exit 1
 fi
 
-# Create group and user for MariaDB
-groupadd mysql
-useradd -g mysql mysql
+# Get the current user
+CURRENT_USER=$(whoami)
 
-# Set MariaDB.repo file path
-REPO_FILE="/etc/yum.repos.d/MariaDB.repo"
+# Define the required username
+TARGET_USER="tuna"
 
-# MariaDB.repo file content
-REPO_CONTENT="[MariaDB-11.4]
-name=MariaDB 11.4 repo (build 46870)
-baseurl=http://mirror.mariadb.org/yum/11.4.2/rockylinux8-amd64/
-gpgcheck=0
-module_hotfixes=1
-"
+# Define the target directory
+OPENSEARCH_HOME="/engn001/opensearch-2.13.0"
 
-# create MariaDB.repo file
-echo "$REPO_CONTENT" | sudo tee $REPO_FILE > /dev/null
-
-# update the dnf cache
-sudo dnf makecache
-
-# check the MariaDB repo
-sudo dnf repolist | grep MariaDB-11.4
-if [ $? -eq 0 ]; then
-    echo "MariaDB repository creation was successful."
-else
-    echo "MariaDB repository creation was FAILED!"
+# Check if the current user matches the required username
+if [ "$CURRENT_USER" != "$TARGET_USER" ]; then
+  echo "This script must be run as user '$TARGET_USER'. Exiting."
+  exit 1
 fi
 
-# install the MariaDB server
-sudo dnf -y install MariaDB
-
-# install the MariaDB client
-sudo dnf -y install mariadb
-
-# Check if MariaDB is installed by looking for the mariadb service
-if sudo systemctl list-units --type=service --all | grep -q 'mariadb.service'; then
-  echo "MariaDB is installed."
-else
-  echo "MariaDB is not installed."
+# Check if the current user has sudo privileges
+if ! sudo -l >/dev/null 2>&1; then
+  echo "This script requires sudo privileges. Exiting."
+  exit 1
 fi
 
-# Configure the MariaDB service to start automatically at boot
-sudo systemctl enable mariadb
+# Swapoff
+sudo swapoff -a
 
-# Start MariaDB service
-sudo systemctl start mariadb
-
-# Define the new root password
-ROOT_PASSWORD="time2013"
-
-# Execute SQL commands to configure MariaDB
-sudo mysql -u root <<EOF
-
-ALTER USER 'root'@'localhost' IDENTIFIED BY '${ROOT_PASSWORD}';
-DELETE FROM mysql.user WHERE User='';
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '${ROOT_PASSWORD}' WITH GRANT OPTION;
-DROP DATABASE IF EXISTS test;
-DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
-FLUSH PRIVILEGES;
-
-grant all on nxm.* to nxm@localhost identified by 'dprtm2017!' ;
-grant all on nxm.* to nxm@'127.0.0.1' identified by 'dprtm2017!' ;
-GRANT FILE ON *.* TO nxm@localhost;
-GRANT FILE ON *.* TO nxm@'127.0.0.1';
-flush privileges;
-
-EOF
-
-# Update the MariaDB configuration to allow remote access
-if grep -q "^bind-address" /etc/my.cnf /etc/my.cnf.d/server.cnf; then
-  sed -i '/^bind-address/s/^/#/' /etc/my.cnf /etc/my.cnf.d/server.cnf
-else
-  echo "bind-address not found, assuming remote access is already allowed."
+# Check if the setting already exists in sysctl.conf
+if ! grep -q "vm.max_map_count=262144" /etc/sysctl.conf; then
+  # Add the setting to sysctl.conf
+  echo "vm.max_map_count=262144" | sudo tee -a /etc/sysctl.conf
 fi
 
-# Restart MariaDB service to apply changes
-sudo systemctl restart mariadb
+# Reload the kernel parameters using sysctl
+sudo sysctl -p
 
-echo "MariaDB configuration completed."
+echo "vm.max_map_count has been set to 262144 and made permanent."
+
+# Add limits
+echo "$TARGET_USER soft nproc 65536" | sudo tee -a /etc/security/limits.conf
+echo "$TARGET_USER hard nproc 65536" | sudo tee -a /etc/security/limits.conf
+echo "$TARGET_USER soft nofile 65536" | sudo tee -a /etc/security/limits.conf
+echo "$TARGET_USER hard nofile 65536" | sudo tee -a /etc/security/limits.conf
+echo "$TARGET_USER soft memlock unlimited" | sudo tee -a /etc/security/limits.conf
+echo "$TARGET_USER hard memlock unlimited" | sudo tee -a /etc/security/limits.conf
+
+# Create the directory with sudo and set the owner to the current user
+sudo mkdir -p $OPENSEARCH_HOME
+sudo chown $TARGET_USER:$TARGET_USER $OPENSEARCH_HOME
+
+echo "Extracting OpenSearch to $OPENSEARCH_HOME."
+
+# Get the directory where the script is located
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Extract the opensearch-2.13.0.tar.gz file into the target directory
+tar -xzf $SCRIPT_DIR/opensearch-2.13.0-linux-x64.tar.gz -C $OPENSEARCH_HOME --strip-components=1
+
+echo "OpenSearch has been extracted to $OPENSEARCH_HOME and ownership set to $CURRENT_USER."
+
+# Define the path to the opensearch.yml file
+CONFIG_FILE="/engn001/opensearch-2.13.0/config/opensearch.yml"
+
+# Check for the OPENSEARCH_DATA_DIR environment variable
+if [ -n "$OPENSEARCH_DATA_DIR" ]; then
+  DATA_DIR="$OPENSEARCH_DATA_DIR"
+elif [ -d "/data001/opensearch" ]; then
+  DATA_DIR="/data001/opensearch"
+else
+  DATA_DIR=""
+fi
+
+# If DATA_DIR is set, ensure tuna user and group can write to it
+if [ -n "$DATA_DIR" ]; then
+  sudo chown -R $TARGET_USER:$TARGET_USER "$DATA_DIR"
+  sudo chmod -R 775 "$DATA_DIR"
+fi
+
+# Check for the OPENSEARCH_LOGS_DIR environment variable
+if [ -n "$OPENSEARCH_LOGS_DIR" ]; then
+  LOGS_DIR="$OPENSEARCH_LOGS_DIR"
+elif [ -d "/logs001/opensearch" ]; then
+  LOGS_DIR="/logs001/opensearch"
+else
+  LOGS_DIR=""
+fi
+
+# If LOGS_DIR is set, ensure tuna user and group can write to it
+if [ -n "$LOGS_DIR" ]; then
+  sudo chown -R $TARGET_USER:$TARGET_USER "$LOGS_DIR"
+  sudo chmod -R 775 "$LOGS_DIR"
+fi
+
+# Use sed to modify the specified lines in the configuration file
+sudo sed -i 's/^#cluster\.name:.*/cluster.name: tuna-ee/' "$CONFIG_FILE"
+sudo sed -i 's/^#node\.name:.*/node.name: single-node/' "$CONFIG_FILE"
+
+# Conditionally set path.data if DATA_DIR is not empty
+if [ -n "$DATA_DIR" ]; then
+  sudo sed -i "s|^#path\.data:.*|path.data: $DATA_DIR|" "$CONFIG_FILE"
+else
+  echo "No valid data directory found. path.data will not be set."
+fi
+
+# Conditionally set path.logs if LOGS_DIR is not empty
+if [ -n "$LOGS_DIR" ]; then
+  sudo sed -i "s|^#path\.logs:.*|path.logs: $LOGS_DIR|" "$CONFIG_FILE"
+else
+  echo "No valid logs directory found. path.logs will not be set."
+fi
+
+sudo sed -i 's/^#network\.host:.*/network.host: 0.0.0.0/' "$CONFIG_FILE"
+
+echo ""                                >> $CONFIG_FILE
+echo "# Configure for TunA EE"         >> $CONFIG_FILE
+echo "discovery.type: single-node"     >> $CONFIG_FILE
+echo "plugin.security.disabled: true"  >> $CONFIG_FILE
+
+# Append security policy configuration
+POLICY_FILE="$OPENSEARCH_HOME/config/opensearch-performance-analyzer/opensearch_security.policy"
+echo "grant {"                                                   >> $POLICY_FILE
+echo "  java.lang.RuntimePermission \"accessUserInformation\";"  >> $POLICY_FILE
+echo "};"                                                        >> $POLICY_FILE
+
+echo "OpenSearch Configuration has been added to opensearch.yml and opensearch_security.policy."
+
+# Start the OpenSearch
